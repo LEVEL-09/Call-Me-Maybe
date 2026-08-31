731faa75ce6c130a27af496e069efc9406b54e8d
Leon <hichamohicham651@gmail.com>
2026-08-31 07:59:30 +0100

    feat: parsing argument use argparse module

--- src/main.py
+++ src/main.py
@@ -1,2 +1,30 @@
+from argparse import ArgumentParser, Namespace
+
+
+DEFAULT_FUNCTIONS_DEFINITION = "data/input/functions_definition.json"
+DEFAULT_INPUT = "data/input/function_calling_tests.json"
+DEFAULT_OUTPUT = "data/output/function_calling_results.json"
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--functions_definition",
+        type=str,
+        default=DEFAULT_FUNCTIONS_DEFINITION,
+    )
+    parser.add_argument(
+        "--input",
+        type=str,
+        default=DEFAULT_INPUT,
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        default=DEFAULT_OUTPUT,
+    )
+    return parser.parse_args()
+
+
 def main() -> None:
-    print("Hello from call-me-maybe!")
+    pass
