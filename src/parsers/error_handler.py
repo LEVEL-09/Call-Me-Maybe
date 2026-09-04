1b0bfe19f8d54f9fcdc88e7b45ed61388526f7ff
Leon <hichamohicham651@gmail.com>
2026-09-04 05:03:47 +0100

    feat: add custom context manager to handler parsing errors

---
+++ src/parsers/error_handler.py
@@ -0,0 +1,32 @@
+import json
+import sys
+from types import TracebackType
+from typing import Self
+
+from pydantic import ValidationError
+
+
+class FileParsingErrorHandler:
+    """Context manager to catch OSError, JSONDecodeError,
+    or ValidationError."""
+
+    def __init__(self) -> None:
+        self.exception = (OSError, json.JSONDecodeError, ValidationError)
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> bool:
+        if exc_type is None:
+            return True
+
+        if issubclass(exc_type, self.exception):
+            print(exc_val)
+            sys.exit(1)
+
+        return False
